b5657dea82143692e15e9ead695365e607f0f0c4
x <hirobrajne123@gmail.com>
2025-01-24 01:34:03 +0100

    Update id.js

--- assets/id.js
+++ assets/id.js
@@ -108,4 +108,16 @@ function setData(id, value){
   document.getElementById(id).innerHTML = value;
 
 }
+// Funkcja do zamknięcia zakładki
+function deleteTab() {
+    // Sprawdzenie, czy karta może być zamknięta
+    if (window.close) {
+        // Próba zamknięcia karty
+        window.close();
+    } else {
+        // Jeśli zamknięcie się nie powiedzie, pokaż komunikat
+        alert("Nie można zamknąć tej karty automatycznie. Zamknij ją ręcznie.");
+    }
+}
+
 
